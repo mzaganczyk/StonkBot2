058daf6915ef0626e09e9b12c05948ebe370512a
Marcin <marcinzag@yahoo.com>
2021-02-09 04:24:07 +0100

    eloo

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 from flask import Flask, render_template
 from screener import Screener
+from datetime import datetime
 
 tabela = Screener()
 stats = tabela.getData()
 app = Flask(__name__)
 print(stats.to_html())
+czas = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
 
 @app.route('/')
 def renderStats():
-    return render_template('stats.html', data=stats.to_html(classes="table table-hover table-striped"))
+    return render_template('stats.html', data=stats.to_html(classes="table table-hover table-striped"), time=czas)
 
 
 if __name__ == '__main__':
