f9bc5fdec8e6b54f47640e5d74c1431dbaca09a4
Marcin <marcinzag@yahoo.com>
2021-02-13 23:01:57 +0100

    elo

--- app.py
+++ app.py
@@ -1,20 +1,33 @@
+import os
+import time
+
 from flask import Flask, render_template
+
 from screener import Screener
-import time, os
+
+from apscheduler.schedulers.background import BackgroundScheduler
 
 os.environ['TZ'] = 'Poland'
-time.tzset()
-czas = time.strftime('%d/%m/%Y %H:%M:%S')
 
-tabela = Screener()
 
-stats = tabela.getData()
+def getData():
+    czas = time.strftime('%d/%m/%Y -- %H:%M:%S')
+    tabela = Screener()
+    stats = tabela.getData()
+    return [stats, czas]
+
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(func=getData, trigger="interval", seconds=10)
+scheduler.start()
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def renderStats():
-    return render_template('stats.html', data=stats.to_html(classes="table table-hover table-striped"), time=czas)
+    return render_template('stats.html', data=getData()[0].to_html(classes="table table-hover table-striped"),
+                           time=getData()[1])
 
 
 if __name__ == '__main__':
