0a8d3965957b965bddf14ed80688b88a822ef73a
Marcin <marcinzag@yahoo.com>
2021-02-20 17:27:03 +0100

    stonkbot v2

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
-# wersja 14.02.2021
+# wersja 17.02.2021
 import os
 import time
 from flask import Flask, render_template
+from screenerAPI import ScreenerAPI
 from screener import Screener
 from flask_apscheduler import APScheduler
 import logging
-
-os.environ['TZ'] = 'Poland'
-time.tzset()
 
 sched = APScheduler()
 app = Flask(__name__)
@@ -17,27 +15,40 @@ class Config(object):
     SCHEDULER_API_ENABLED = True
 
 
-czas = time.strftime('%d/%m/%Y -- %H:%M:%S CET')
-stats = Screener().getData()
+czas = time.strftime('%d/%m/%Y -- %H:%M:%S UTC')
+stats = ScreenerAPI().getHTML()
+statsOld = Screener().getData()
 
 
-@sched.task('cron', id='update', hour=14)
+@sched.task('cron', id='update', hour=13, minute=00)
 def getData():
     global czas, stats
-    czas = time.strftime('%d/%m/%Y -- %H:%M:%S CET')
-    tabela = Screener()
-    stats = tabela.getData()
+    czas = time.strftime('%d/%m/%Y -- %H:%M:%S UTC')
+    tabela = ScreenerAPI()
+    stats = tabela.getHTML()
 
 
 @app.route('/')
 def renderStats():
-    return render_template('stats.html', data=stats.to_html(classes="table table-hover table-striped"),
+    return render_template('stats.html',
+                           data=stats,
                            time=czas)
 
 
 @app.route('/about')
 def renderAbout():
     return render_template('about.html')
+
+
+@app.route('/help')
+def renderHelp():
+    return render_template('help.html')
+
+
+@app.route('/archive')
+def renderArchive():
+    stonklist = os.listdir('static/archive')
+    return render_template('archive.html', len=len(stonklist), stonklist=stonklist)
 
 
 if __name__ == '__main__':
